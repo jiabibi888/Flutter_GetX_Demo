50987b904426ac9b2a5386621c845d7d7d4cab6e
Jabir <zjb11934@ly.com>
2021-10-12 17:44:53 +0800

    obx响应式状态管理

--- getx_demo/lib/home_view.dart
+++ getx_demo/lib/home_view.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_demo/model/person_model.dart';
 
+// ignore: must_be_immutable
 class HomeView extends StatelessWidget {
+  // var person = Person();
 
-  const HomeView({Key? key}) : super(key: key);
+  final person = Person(name: "Jabir", age: 18).obs;
+  HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('首页')),
       body: Center(
-        child: Column(
-          children: [
-            Text(
-              Get.arguments ?? "无参数传递过来"
-            ),
-            ElevatedButton(
+          child: Column(
+        children: [
+          Text(Get.arguments ?? "无参数传递过来"),
+          ElevatedButton(
               onPressed: () {
                 //返回不带参数
                 Get.back();
@@ -24,11 +26,21 @@ class HomeView extends StatelessWidget {
                 // Get.back(
                 //   result: '返回的参数就是我'
                 // );
-            }, 
-            child: Text('返回上一页'))
-          ],
-          )
-        ),
+              },
+              child: Text('返回上一页')),
+          ElevatedButton(
+              onPressed: () {
+                // person.name.value = person.name.value.toUpperCase();
+
+                person.update((val) {
+                  person.value.name = person.value.name.toString().toUpperCase();
+                });
+              },
+              child: Text("转换为大写")),
+          // Obx(() => Text(person.name.value))
+          Obx(() => Text(person.value.name))
+        ],
+      )),
     );
   }
 }
---
+++ getx_demo/lib/model/person_model.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class Person {
+  //rx 变量
+  // var name = 'Jabir'.obs;
+  // var age = 18.obs;
+
+  //构造函数创建
+  var name;
+  var age;
+  Person({this.name, this.age});
+
+}
